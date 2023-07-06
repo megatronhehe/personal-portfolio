3879bbc1745ca8896d49a99c55c152358145687e
Megatronhehee <satyamahendra09@gmail.com>
2023-07-06 09:54:42 +0700

    new design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,21 @@ import React from "react";
 const Header = () => {
 	return (
 		<header className="flex flex-col items-center px-4 text-gray-500 sm:justify-between sm:flex-row sm:border-b">
-			<h1 className="w-full py-2 text-xl font-semibold tracking-wide text-center border-b sm:border-none sm:w-0">
+			<h1 className="w-full py-2 text-xl font-semibold tracking-wide text-center text-blue-500 border-b sm:border-none sm:w-0">
 				satya.dev
 			</h1>
 			<nav>
 				<ul className="flex text-sm sm:gap-8">
-					<li className="px-4 py-1 border sm:px-0 rounded-bl-xl sm:border-none">
+					<li className="px-4 py-1 border-b border-l sm:px-0 rounded-bl-xl sm:border-none">
 						home
 					</li>
-					<li className="px-4 py-1 border sm:px-0 sm:border-none">about</li>
-					<li className="px-4 py-1 border sm:px-0 sm:border-none">projects</li>
-					<li className="px-4 py-1 border sm:px-0 sm:border-none rounded-br-xl">
+					<li className="px-4 py-1 border-b border-l sm:px-0 sm:border-none">
+						about
+					</li>
+					<li className="px-4 py-1 border-b border-l sm:px-0 sm:border-none">
+						projects
+					</li>
+					<li className="px-4 py-1 border-b border-x sm:px-0 sm:border-none rounded-br-xl">
 						contact
 					</li>
 				</ul>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,58 @@
 import React from "react";
-import satya from "../assets/satyajpg.jpg";
+import satya from "../assets/satyajpg2.jpg";
 
 import { FaReact, FaHtml5, FaCss3Alt, FaJsSquare } from "react-icons/fa";
 import { BiLogoTailwindCss } from "react-icons/bi";
 
 const Home = () => {
 	return (
-		<div className="relative flex flex-col items-center justify-center py-4 mt-16 text-gray-500 border">
-			<div className="flex justify-around text-4xl">
-				<p>: : : _</p>
-				<img
-					src={satya}
-					className="object-contain w-1/2 shadow-lg rounded-2xl"
-				/>
-				<div className="self-end">
-					<p>x x _</p>
-					<p>_ : : </p>
-				</div>
-			</div>
-
-			<div className="w-3/4 p-2 mt-4 border rounded-xl">
-				<h1 className="text-xl font-semibold">
-					: : Front-end React Web Developer
-				</h1>
-				<div className="text-sm">
-					<p className="mt-4">
-						Hi, I'm <span className="font-semibold">Satya Mahendra,</span>
+		<div className="flex justify-center tracking-wide">
+			<div className="max-w-xl p-8 text-gray-500 ">
+				<div className="px-8 border-l-4 border-blue-400">
+					<p>Hi, I'm</p>
+					<h1 className="pb-2 mb-4 text-2xl text-blue-500 border-b border-blue-500">
+						Satya Mahendra
+					</h1>
+					<div className="flex justify-center gap-4 text-2xl">
+						<div>
+							<p>x _</p>
+							<p>: : _</p>
+						</div>
+						<img src={satya} className="w-2/3 rounded-2xl" />
+						<div className="self-end">
+							<p>: : _</p>
+							<p>x _</p>
+						</div>
+					</div>
+					<h2 className="my-4 text-lg font-semibold">
+						Frontend <span className="font-bold text-blue-300">React</span> Web
+						Dev.
+					</h2>
+					<p className="text-center">
+						I'm a passionate frontend React web developer based in Malang, East
+						Java, Indonesia.
 					</p>
-					<p>
-						I'm a front-end React JS web developer based in Malang, East Java.
-					</p>
+					<p className="my-4 text-end">. . . nice to meet you!</p>
 				</div>
-			</div>
-
-			<div className="flex items-center mt-4">
-				<h2 className="px-4 border-r-2 border-red-500">Tech Stack</h2>
-				<div className="flex gap-4 ml-4">
-					<FaHtml5 size="25" color="orange" />
-					<FaCss3Alt size="25" color="blue" />
-					<FaJsSquare size="25" color="orange" />
-					<FaReact size="25" color="lightblue" />
-					<BiLogoTailwindCss size="25" color="purple" />
+				<div className="flex items-center justify-between p-2 pl-4 text-white bg-blue-400 shadow-sm rounded-xl">
+					<p>Tech Stack</p>
+					<div className="flex justify-center gap-4 p-2 bg-white rounded-lg sm:gap-8">
+						<div>
+							<FaHtml5 size="30" color="orange" />
+						</div>
+						<div>
+							<FaCss3Alt size="30" color="blue" />
+						</div>
+						<div>
+							<FaJsSquare size="30" color="orange" />
+						</div>
+						<div>
+							<FaReact size="30" color="lightblue" />
+						</div>
+						<div>
+							<BiLogoTailwindCss size="30" color="purple" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
