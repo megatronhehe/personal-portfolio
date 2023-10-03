ebced99d72847a3b612937dcaff856a45aa0d0b3
Megatronhehee <satyamahendra09@gmail.com>
2023-10-03 11:26:37 +0700

    feature added: current page indicator on navbar

--- src/components/NavbarButton.jsx
+++ src/components/NavbarButton.jsx
@@ -25,20 +25,30 @@ const NavbarButton = ({
 			}}
 			className="relative flex items-center justify-center w-10 h-10 text-2xl duration-200 hover:scale-125 sm:text-4xl"
 		>
-			{icon}
+			{({ isActive }) => (
+				<>
+					{icon}
 
-			<AnimatePresence>
-				{showSectionName && (
-					<motion.div
-						initial={{ opacity: 0, y: 10 }}
-						animate={{ opacity: 1, y: 0 }}
-						exit={{ opacity: 0, y: 10 }}
-						className="absolute px-3 py-1 text-sm text-white bg-gray-700 border sm:text-base -top-6 sm:-top-12 rounded-xl dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600"
-					>
-						{sectionName}
-					</motion.div>
-				)}
-			</AnimatePresence>
+					{isActive && !isPopup ? (
+						<PiDiamondFill className="absolute text-base text-blue-400 -top-3 animate-bounce" />
+					) : (
+						""
+					)}
+
+					<AnimatePresence>
+						{showSectionName && (
+							<motion.div
+								initial={{ opacity: 0, y: 10 }}
+								animate={{ opacity: 1, y: 0 }}
+								exit={{ opacity: 0, y: 10 }}
+								className="absolute px-3 py-1 text-sm text-white bg-gray-700 border sm:text-base -top-6 sm:-top-12 rounded-xl dark:bg-gray-800 dark:text-gray-200 dark:border-gray-600"
+							>
+								{sectionName}
+							</motion.div>
+						)}
+					</AnimatePresence>
+				</>
+			)}
 		</NavLink>
 	);
 };
