73d28d4336e343945571ca14e959933e347a6899
Megatronhehee <satyamahendra09@gmail.com>
2023-10-07 20:27:45 +0700

    fix: github repo button animation removed because its annoying

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -23,7 +23,7 @@ const About = () => {
 		>
 			<div className="w-full h-full max-w-3xl">
 				<section className="flex flex-col items-center justify-center border-b dark:border-gray-600 h-1/3">
-					<h2 className="flex flex-col items-center justify-end gap-2 text-lg text-center sm:text-2xl h-1/2">
+					<h2 className="relative flex flex-col items-center justify-end gap-2 text-lg text-center sm:text-2xl h-1/2">
 						<PiStackLight className="w-10 h-10 p-2 border rounded-full dark:border-gray-600 sm:w-14 sm:h-14" />
 						<span>Projects</span>
 					</h2>
@@ -36,7 +36,7 @@ const About = () => {
 						<a
 							href="https://github.com/megatronhehe?tab=repositories"
 							target="_blank"
-							className="flex items-center gap-2 font-normal duration-200 hover:font-bold animate-bounce hover:text-blue-400"
+							className="flex items-center gap-2 font-normal duration-200 hover:font-bold  hover:text-blue-400"
 						>
 							my github repo <PiArrowSquareOutLight className="text-xl" />
 						</a>
