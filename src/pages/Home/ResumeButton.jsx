65d651c9110726cdbc28057e8ed0c0f5a9b43905
Megatronhehe <satyamahendra09@gmail.com>
2023-12-16 19:31:45 +0700

    update: updated cv links

--- src/pages/Home/ResumeButton.jsx
+++ src/pages/Home/ResumeButton.jsx
@@ -23,14 +23,14 @@ const ResumeButton = () => {
 							className="absolute flex justify-center gap-2 p-2 text-base bg-gray-600 rounded-full -top-14"
 						>
 							<a
-								href="https://drive.google.com/file/d/1HV4Q1VpUxc2RdpUV2SzfXNcB82U3V4oj/view?usp=drive_link"
+								href="https://drive.google.com/file/d/1MWMsh4ilYGUpq7uRPUI6_maKg0fnICvI/view?usp=drive_link"
 								target="_blank"
 								className="flex items-center justify-center w-10 h-10 p-2 font-semibold duration-200 bg-blue-300 rounded-l-full hover:bg-white hover:text-blue-300"
 							>
 								EN
 							</a>
 							<a
-								href="https://drive.google.com/file/d/1Y-DYyAhOPSlvdL3SZke4MF2fCM-E9h2X/view?usp=drive_link"
+								href="https://drive.google.com/file/d/1mKGAvG3aw02XG_QKUkAz_hN8Tyml0-RE/view?usp=drive_link"
 								target="_blank"
 								className="flex items-center justify-center w-10 h-10 p-2 font-semibold duration-200 bg-red-300 rounded-r-full hover:text-red-300 hover:bg-white"
 							>
